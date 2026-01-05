3482a368ecf3d3911ed0ef7240be9076f75c0199
hohll <laurent.hohl@gmail.com>
2026-01-05 15:44:40 +0100

    LUT-30415 : Add icons to breadcrumbaccordion right feature

--- src/sql/plugins/forms/modules/breadcrumbaccordion/upgrade/update_db_breadcrumbaccordion1.0.0-2.0.0.sql
+++ src/sql/plugins/forms/modules/breadcrumbaccordion/upgrade/update_db_breadcrumbaccordion1.0.0-2.0.0.sql
@@ -1,4 +1,4 @@
 --liquibase formatted sql
---changeset forms-breadcrumbaccordion:update_db_breadcrumbaccordion1.0.0-2.0.0.sql
+--changeset forms-breadcrumbaccordion:update_db_breadcrumbaccordion-1.0.0-2.0.0.sql
 --preconditions onFail:MARK_RAN onError:WARN
 UPDATE core_admin_right SET icon_url='ti ti-line-dashed' WHERE id_right='BREADCRUMBACCORDION_MANAGEMENT';
